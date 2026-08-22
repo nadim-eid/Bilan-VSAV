0bac7de441eea07b5c71e6f2222c7705db777886
nadim-eid <63366370+nadim-eid@users.noreply.github.com>
2026-08-22 23:47:07 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,4 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import BilanVsav from './BilanVsav.jsx';
 import './index.css';
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><BilanVsav/></React.StrictMode>);
+import { StatusBar } from '@capacitor/status-bar';
+
+StatusBar.hide();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BilanVsav />
+  </React.StrictMode>
+);
